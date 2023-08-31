02bd779a3b3dd61f71b2b94339b36e5b9ac4a7f8
nvquy <ngoq753429@gmail.com>
2023-08-31 13:41:30 +0700

    update file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Radio from "./component/base/Radio";
+import Checkbox from "./component/base/Checkbox";
+import TodoList from "./component/TodoList";
+
+export default function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
